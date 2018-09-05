8938e0584d872d38775356795ac707f98183a6eb
andi <amayer80@gmail.com>
2018-09-05 18:40:31 +0200

    first commit

--- src/main/java/andi/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/andi/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package andi.springframework.spring5webapp.bootstrap;
 
 import andi.springframework.spring5webapp.model.Author;
 import andi.springframework.spring5webapp.model.Book;
+import andi.springframework.spring5webapp.model.Publisher;
 import andi.springframework.spring5webapp.repositories.AuthorRepository;
 import andi.springframework.spring5webapp.repositories.BookRepository;
+import andi.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository,
+                        BookRepository bookRepository,
+                        PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,8 +32,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData() {
 
+        Publisher publisher = new Publisher();
+        publisher.setName("Frodo");
+
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain driven design", "1234", "Harper Collins");
+        Book ddd = new Book("Domain driven design", "1234",publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -35,7 +46,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         bookRepository.save(ddd);
 
         Author john = new Author("Jone ", "News");
-        Book noeBJ = new Book("Web driven design", "8234", "John Hohn Collins");
+        Book noeBJ = new Book("Web driven design", "8234", publisher);
         eric.getBooks().add(noeBJ);
         ddd.getAuthors().add(john);
 
---
+++ src/main/java/andi/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package andi.springframework.spring5webapp.repositories;
+
+import andi.springframework.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
